1e7e09b667c6ac9a835332486fee34ec13e73556
JamalSadiqueDonkor <jamal.sadique.donkor@gmail.com>
2024-01-05 23:59:31 -0500

    fix(query): make latest-invoices component output more than one user data

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -34,12 +34,20 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+    // const data = await sql<LatestInvoiceRaw>`
+    //   SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
+    //   FROM invoices
+    //   JOIN customers ON invoices.customer_id = customers.id
+    //   ORDER BY invoices.date DESC
+    //   LIMIT 5`;
+
     const data = await sql<LatestInvoiceRaw>`
-      SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
-      FROM invoices
+      SELECT DISTINCT ON (customers.id) invoices.amount, customers.name, customers.image_url, 
+        customers.email, invoices.id FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
-      ORDER BY invoices.date DESC
+      ORDER BY customers.id, invoices.date DESC
       LIMIT 5`;
+
 
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
