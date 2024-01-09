f9ea14520327608098333af94bfd4d91dd8647c0
JamalSadiqueDonkor <jamal.sadique.donkor@gmail.com>
2024-01-09 17:33:22 -0500

    fix: Replace single quotes with backticks for console.log to work properly + remove extra console.log().

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -9,7 +9,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   function handleSearch(term: string) {
-    console.log('Searching... ${term}');
+    console.log(`Searching... ${term}`);
 
     const params = new URLSearchParams(searchParams);
     if (term) {
@@ -18,7 +18,6 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-    console.log(term);
   }
 
   return (
