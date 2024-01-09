f99ca6039e5761984d8abca631249cd15fb61a69
JamalSadiqueDonkor <jamal.sadique.donkor@gmail.com>
2024-01-09 17:19:54 -0500

    Implement debouncing to optimize search feature.

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -9,6 +9,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   function handleSearch(term: string) {
+    console.log('Searching... ${term}');
+
     const params = new URLSearchParams(searchParams);
     if (term) {
       params.set('query', term);
