89380ada48e735f50cb458f53a43d4c4a1c084f9
JamalSadiqueDonkor <jamal.sadique.donkor@gmail.com>
2024-01-09 17:01:53 -0500

    feat: Add feature to update url with search params.

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,9 +1,21 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
   function handleSearch(term: string) {
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
     console.log(term);
   }
 
