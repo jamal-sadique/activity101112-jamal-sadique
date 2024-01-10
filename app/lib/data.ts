9041eab6954fe5889426b25979e5d6952154d356
JamalSadiqueDonkor <jamal.sadique.donkor@gmail.com>
2024-01-10 12:08:47 -0500

    Modify sql query.

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -37,24 +37,24 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   noStore();
   try {
-    // const data = await sql<LatestInvoiceRaw>`
-    //   SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
-    //   FROM invoices
-    //   JOIN customers ON invoices.customer_id = customers.id
-    //   ORDER BY invoices.date DESC
-    //   LIMIT 5`;
+    const data = await sql<LatestInvoiceRaw>`
+      SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
+      FROM invoices
+      JOIN customers ON invoices.customer_id = customers.id
+      ORDER BY invoices.date DESC
+      LIMIT 5`;
 
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
     console.log('Fetching latest invoices data...');
     await new Promise((resolve) => setTimeout(resolve, 3000));
     
-    const data = await sql<LatestInvoiceRaw>`
-      SELECT DISTINCT ON (customers.id) invoices.amount, customers.name, customers.image_url, 
-        customers.email, invoices.id FROM invoices
-      JOIN customers ON invoices.customer_id = customers.id
-      ORDER BY customers.id, invoices.date DESC
-      LIMIT 5`;
+    // const data = await sql<LatestInvoiceRaw>`
+    //   SELECT DISTINCT ON (customers.id) invoices.amount, customers.name, customers.image_url, 
+    //     customers.email, invoices.id FROM invoices
+    //   JOIN customers ON invoices.customer_id = customers.id
+    //   ORDER BY customers.id, invoices.date DESC
+    //   LIMIT 5`;
 
 
     const latestInvoices = data.rows.map((invoice) => ({
