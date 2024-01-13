52fff38d98bb11aa9f4569f5ed6d79cb9e7c8476
JamalSadiqueDonkor <jamal.sadique.donkor@gmail.com>
2024-01-12 20:36:02 -0500

    feat(invoices):  Update page component to receive 'params' prop.

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,7 +2,9 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers } from '@/app/lib/data';
  
-export default async function Page() {
+export default async function Page({ params }: { params: { id: string } }) {
+  const id = params.id;
+  
   return (
     <main>
       <Breadcrumbs
