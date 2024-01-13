4208b4ce9d37991b6618df4621b955b672807621
JamalSadiqueDonkor <jamal.sadique.donkor@gmail.com>
2024-01-12 20:50:05 -0500

    feat(invoices): Add functions to fetch the specific invoice and customers.

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,10 +1,14 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
-import { fetchCustomers } from '@/app/lib/data';
+import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
  
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
-  
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+
   return (
     <main>
       <Breadcrumbs
