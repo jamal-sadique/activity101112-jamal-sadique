ff3b7e6a21c1ed1c0e0511cc14cc16eb2866a325
JamalSadiqueDonkor <jamal.sadique.donkor@gmail.com>
2024-01-27 18:39:24 -0500

    feat: Add hero image.

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
-import Image from 'next/image';
+import HeroImage from '@/app/ui/hero-image';
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-primary p-4 md:h-52">
         <AcmeLogo />
         {/*  */}
       </div>
@@ -30,21 +30,7 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
-          <Image
-            src="/hero-desktop.png"
-            width={1000}
-            height={760}
-            className="hidden md:block"
-            alt="Screenshots of the dashboard project showing desktop version"
-          />
-          <Image
-            src="/hero-mobile.png"
-            width={560}
-            height={620}
-            className="block md:hidden"
-            alt="Screenshots of the dashboard project showing desktop version"
-          />
+          <HeroImage />
         </div>
       </div>
     </main>
