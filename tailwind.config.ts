4f3d1f4222703a86668e4431a470064b4d661611
JamalSadiqueDonkor <jamal.sadique.donkor@gmail.com>
2024-01-27 18:42:36 -0500

    Modify daisyui configuration for easier theming.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,12 +27,18 @@ const config: Config = {
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require("daisyui")
-  ],
+  plugins: [require('@tailwindcss/forms'), require("daisyui")],
   daisyui: {
-    themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    themes: [
+      {
+        dark: {
+          ...require('daisyui/src/theming/themes')['dark'],
+        },
+        light: {
+          ...require('daisyui/src/theming/themes')['light'],
+        },
+      },
+    ],
     darkTheme: "dark", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
