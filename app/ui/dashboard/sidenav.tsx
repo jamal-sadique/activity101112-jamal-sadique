b98f26a569654df26bea3ae88c5013dfae413945
JamalSadiqueDonkor <jamal.sadique.donkor@gmail.com>
2024-01-28 14:06:11 -0500

    Add semantic colors to the side-nav.

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -17,14 +17,14 @@ export default function SideNav() {
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
-        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:flex md:flex-col md:items-center md:justify-end"> 
+        <div className="hidden h-auto w-full grow rounded-md bg-primary-content/20 md:flex md:flex-col md:items-center md:justify-end"> 
         <div className="md:flex-grow"></div>
         </div>
         <form action={async () => {
             'use server';
             await signOut();
           }}>
-          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-primary-content/20 p-3 text-sm font-medium hover:bg-primary-content hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
           </button>
